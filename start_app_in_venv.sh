f5b01ed1453fd7d111307ce39fde4374ef8d6e14
Shariff Navab <shariff.navab@opstree.com>
2025-06-28 15:44:54 +0530

    updated some files

--- start_app_in_venv.sh
+++
@@ -1,8 +0,0 @@
-   #!/bin/bash
-
-   # Source the virtual environment activation script
-   source /opt/my_flask_app/venv/bin/activate
-
-   # Execute the Flask application using the venv's python
-   exec python3 /opt/my_flask_app/app.py
-   
